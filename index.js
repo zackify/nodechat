75e7a188d1923d1447d29df3375e1267695d12e3
Zach <zackify@gmail.com>
2014-02-19 12:47:29 -0800

    added markdown support and stripping of html tags

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var markdown = require( "markdown" ).markdown;
 var app = express();
 var port = process.env.PORT || 80;
 app.set('views', __dirname + '/tpl');
@@ -14,8 +15,10 @@ app.get("/", function(req, res){
 app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
+    socket.emit('message', { message: 'welcome (you can use <a href="http://daringfireball.net/projects/markdown/">markdown</a>), view the source <a href="https://github.com/zackify/nodechat">on my github</a>' });
     socket.on('send', function (data) {
+        var message = data.message.replace(/(<([^>]+)>)/ig,"");
+        data.message = markdown.toHTML(message);
         io.sockets.emit('message', data);
     });
 });
